fada68c5050740c28d8f4d5f409cae225fa5acaa
ClemBach54 <Clement.bachau@gmail.com>
2024-02-09 12:51:28 +0100

    Lieu et ville

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VilleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VilleRepository::class)]
@@ -18,6 +20,14 @@ class Ville
 
     #[ORM\Column(length: 255)]
     private ?string $codePostal = null;
+
+    #[ORM\OneToMany(targetEntity: Lieu::class, mappedBy: 'ville')]
+    private Collection $Lieu;
+
+    public function __construct()
+    {
+        $this->Lieu = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,36 @@ class Ville
     public function setCodePostal(string $codePostal): static
     {
         $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Lieu>
+     */
+    public function getLieu(): Collection
+    {
+        return $this->Lieu;
+    }
+
+    public function addLieu(Lieu $lieu): static
+    {
+        if (!$this->Lieu->contains($lieu)) {
+            $this->Lieu->add($lieu);
+            $lieu->setVille($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLieu(Lieu $lieu): static
+    {
+        if ($this->Lieu->removeElement($lieu)) {
+            // set the owning side to null (unless already changed)
+            if ($lieu->getVille() === $this) {
+                $lieu->setVille(null);
+            }
+        }
 
         return $this;
     }
