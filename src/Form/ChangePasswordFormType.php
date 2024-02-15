d3a74c037060c0b111f07c1698e1d3c74d22a532
Baptiste Duprat <150901157+baptiste-duprat@users.noreply.github.com>
2024-02-15 16:42:16 +0100

    Modification du mot de passe utilisateur

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -9,18 +9,21 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Security\Core\Validator\Constraints as SecurityAssert;
+
 
 class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class, [
+            ->add('oldPassword', PasswordType::class, [
                 'trim'=>true,
-                'invalid_message' => 'Ne correspond pas au mot de passe actuel',
                 'required'=>false,
+                'mapped' => false,
                 'label' => 'Ancien mot de passe',
                 'label_html' => true,
+                'constraints' => [new SecurityAssert\UserPassword(message: "Le mot de passe ne correspond pas au mot de passe actuel")]
             ])
             ->add('motDePasse', RepeatedType::class, [
                 'type'=> PasswordType::class,
--- src/Form/SortiesType.php
+++ src/Form/SortiesType.php
@@ -10,12 +10,16 @@ use App\Entity\User;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortiesType extends AbstractType
@@ -26,15 +30,15 @@ class SortiesType extends AbstractType
             ->add('nom', TextType::class, [
                 'label' => 'Nom de la sortie'
             ])
-            ->add('dateHeureDebut',DateTimeType::class, [
-                'label'=>'Date et heure de la sortie',
-                'html5'=>true,
-                'widget'=>'single_text',
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie',
+                'html5' => true,
+                'widget' => 'single_text',
             ])
-            ->add('dateLimiteInscription',DateTimeType::class, [
-                'label'=>"Date limite d'inscription",
-                'html5'=>true,
-                'widget'=>'single_text'
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => "Date limite d'inscription",
+                'html5' => true,
+                'widget' => 'single_text'
             ])
             ->add('nbInscriptionsMax', TextType::class, [
                 'label' => 'Nombre de places'
@@ -42,18 +46,68 @@ class SortiesType extends AbstractType
             ->add('duree', IntegerType::class, [
                 'label' => 'Durée (en minutes)'
             ])
-            ->add('infosSortie', TextareaType::class,[
-                'label'=>"Description et infos",
+            ->add('infosSortie', TextareaType::class, [
+                'label' => "Description et infos",
             ])
             ->add('ville', EntityType::class, [
                 'class' => Ville::class,
                 'choice_label' => 'nom',
-                'mapped' => false
-            ])
-            ->add('lieu', EntityType::class, [
-                'class' => Lieu::class,
-                'choice_label' => 'nom',
-            ])
+                'mapped' => false,
+                'choice_value'=> function (?Ville $ville) {
+                    return $ville ? $ville->getId() : '';
+                }
+                    ])
+
+        ->add('save', SubmitType::class, [
+        'label' => 'Enregistrer'
+    ])
+        ->add('saveAndAdd', SubmitType::class, [
+            'label' => 'Publier une sortie'
+        ]);
+
+            $formModifier = function (FormInterface $form, Ville $ville = null): void
+            {
+                $lieu = null === $ville ? [] : $ville->getLieu();
+                $form->add('lieu', EntityType::class, [
+                    'label'=> 'Lieu : ',
+                    'class'=> Lieu::class,
+                    'placeholder'=>'--Sélectionnez un lieu--',
+                    'choices'=>$lieu,
+                    'mapped' => false,
+                ]);
+            };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($formModifier): void
+            {
+                $form = $event->getForm();
+                $ville = $form['ville']->getData();
+                $formModifier($form, $ville);
+            }
+        );
+        $builder->get('ville')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function(FormEvent $event) use ($formModifier): void
+            {
+                $ville = $event->getForm()->getData();
+                $formModifier($event->getForm()->getParent(), $ville);
+            }
+        );
+
+    }
+
+            /*
+            ->add('lieu', TextType::class)
+            ->addEventListener(FormEvents::PRE_SUBMIT, function (PreSubmitEvent $event) use ($builder): void {
+                $data = $event->getData();
+                $form = $event->getForm();
+
+                if (isset($data['lieu'])) {
+                    $ville = $data['ville'];
+                }
+            })
+
             ->add('rue', EntityType::class, [
                 'class' => Lieu::class,
                 'mapped' => false,
@@ -74,13 +128,7 @@ class SortiesType extends AbstractType
                 'mapped' => false,
                 'choice_label' => 'longitude'
             ])
-            ->add('save', SubmitType::class,[
-                'label'=>'Enregistrer'
-            ])
-            ->add('saveAndAdd', SubmitType::class,[
-                'label'=>'Publier une sortie'
-            ]);
-    }
+            */
 
 
     public function configureOptions(OptionsResolver $resolver): void
